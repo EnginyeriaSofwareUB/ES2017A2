504490abc7b5cae3d310c567350a07a3ff05b907
krypth0n <grabioma7@alumnes.ub.edu>
2017-10-09 10:30:08 +0200

    Added clouds

---
+++ ES-A2 Project/Assets/Game Assets/Scripts/Intangible/Cloud.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : Intangible {
+
+	[SerializeField] private float startX = -6f;
+	[SerializeField] private float endX = 6;
+	private float currentPos;
+	[SerializeField] private float speed = 1f;
+
+	[SerializeField] private float height = 4f;
+
+	// Use this for initialization
+	void Start () {		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentPos = transform.position.x;
+
+		float nextPos = currentPos + 0.01f * speed;
+			
+		if (currentPos >= endX) {
+			transform.position = new Vector3 (startX, height, transform.position.z);
+		} else {
+			transform.position = new Vector3 (nextPos, height, transform.position.z);
+		}
+	}
+}
