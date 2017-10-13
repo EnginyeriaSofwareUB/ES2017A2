7675fcb23583b13e74d11fb70f499b28d1ef0319
krypth0n <grabioma7@alumnes.ub.edu>
2017-10-13 16:01:44 +0200

    Added Start and End point objects, collision still missing

--- ES-A2 Project/Assets/Game Assets/Scripts/Intangible/Cloud.cs
+++ ES-A2 Project/Assets/Game Assets/Scripts/Intangible/Cloud.cs
@@ -6,25 +6,32 @@ public class Cloud : Intangible {
 
 	[SerializeField] private float startX = -6f;
 	[SerializeField] private float endX = 6;
-	private float currentPos;
+	private float currentPos = 0;
 	[SerializeField] private float speed = 1f;
 
 	[SerializeField] private float height = 4f;
 
 	// Use this for initialization
-	void Start () {		
+	void Start () {
 	}
+
 
 	// Update is called once per frame
 	void Update () {
-		currentPos = transform.position.x;
+		var start = GameObject.Find ("StartPoint").transform.position;
+		var end = GameObject.Find ("EndPoint").transform.position;
+		var vect = end - start;
+		double nextX = start.x + vect.x*0.005*currentPos;
+		double nextY = start.y + vect.y*0.005*currentPos;
+		double nextZ = start.z + vect.z*0.005*currentPos;
+		transform.position = new Vector3 ((float) nextX, (float) nextY, (float) nextZ);
+		currentPos = currentPos + 1;
 
-		float nextPos = currentPos + 0.01f * speed;
-			
-		if (currentPos >= endX) {
-			transform.position = new Vector3 (startX, height, transform.position.z);
-		} else {
-			transform.position = new Vector3 (nextPos, height, transform.position.z);
+	}
+
+	protected void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "EndPoint") {
+			transform.position = new Vector3(0.0f,0.0f,0.0f);
 		}
 	}
 }
