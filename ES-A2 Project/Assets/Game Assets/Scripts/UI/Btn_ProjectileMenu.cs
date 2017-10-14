5240119af66f9e2cdcb30660d1879e52b371cd98
Ivan Toro <ivan_93_bcn@hotmail.com>
2017-10-14 11:29:11 +0200

    Menu de proyectiles visual, funcionalidades no implementadas

---
+++ ES-A2 Project/Assets/Game Assets/Scripts/UI/Btn_ProjectileMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Btn_ProjectileMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayOnClick()
+	{
+        SceneManager.LoadScene("Test Scene");
+    }
+
+    public void BackOnClick()
+	{
+        SceneManager.LoadScene("IndexMenu");
+    }
+}
