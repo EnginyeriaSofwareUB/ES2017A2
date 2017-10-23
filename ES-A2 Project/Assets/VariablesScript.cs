46fdebd3df1ab8df0a85c177956e694e525df08e
Ivan Toro <ivan_93_bcn@hotmail.com>
2017-10-23 16:45:53 +0200

    Diseño implementado del menu de variables

---
+++ ES-A2 Project/Assets/VariablesScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VariablesScript : MonoBehaviour {
+
+    //[SerializeField] private Text coins;
+
+    public void onChangeMoneySlider()
+    {
+        Text coins = GameObject.Find("TextCounterMoney").GetComponent<Text>();
+        coins.text = ((int)GameObject.Find("SliderMoney").GetComponent<Slider>().value).ToString();
+    }
+}
