9e2ac1a4c60741af98a7fce03a0897a72d4684d1
yoma63 <gevdugaei@hotmail.com>
2017-11-15 03:50:58 +0100

    Crear build del juego

--- ES-A2 Project/Assets/Game Assets/Scripts/Terrain/Terrain2D.cs
+++ ES-A2 Project/Assets/Game Assets/Scripts/Terrain/Terrain2D.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 [RequireComponent(typeof(PolygonCollider2D))]
 [ExecuteInEditMode()]
