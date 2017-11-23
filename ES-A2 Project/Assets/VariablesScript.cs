ad1eb3586860f7549723c5ce8091a8abb28b935b
Ivan Toro <ivan_93_bcn@hotmail.com>
2017-11-23 22:14:24 +0100

    Arreglado errores de varios menus

--- ES-A2 Project/Assets/VariablesScript.cs
+++ ES-A2 Project/Assets/VariablesScript.cs
@@ -7,10 +7,18 @@ public class VariablesScript : MonoBehaviour {
 
     [SerializeField] private Text textCounterMoney;
     [SerializeField] private Slider sliderMoney;
+    [SerializeField] private Text textCounterAnimal;
+    [SerializeField] private Slider sliderAnimal;
 
-    public void onChangeSlider()
+    public void onChangeSliderMoney()
     {
         Text coins = textCounterMoney;
         coins.text = ((int)sliderMoney.value).ToString();
+    }
+
+    public void onChangeSliderAnimal()
+    {
+        Text animals = textCounterAnimal;
+        animals.text = ((int)sliderAnimal.value).ToString();
     }
 }
